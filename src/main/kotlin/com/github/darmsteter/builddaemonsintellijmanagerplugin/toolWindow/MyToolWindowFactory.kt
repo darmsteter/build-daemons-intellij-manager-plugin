6f31674973b107dcc0d5150fa81995af21a8830f
Svetlana Koreneva <corenewa.s@gmail.com>
2023-08-14 23:55:48 +0200

    displaying daemons as a button and a dialog with it

--- src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/toolWindow/MyToolWindowFactory.kt
@@ -9,10 +9,7 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.management.ManagementFactory
 import java.text.DecimalFormat
-import javax.swing.BoxLayout
-import javax.swing.JLabel
-import javax.swing.JPanel
-import javax.swing.SwingUtilities
+import javax.swing.*
 import kotlin.concurrent.fixedRateTimer
 
 class MyToolWindowFactory : ToolWindowFactory {
@@ -26,30 +23,20 @@ class MyToolWindowFactory : ToolWindowFactory {
     class MyToolWindow {
         private val panel = JPanel()
         private val layout = BoxLayout(panel, BoxLayout.Y_AXIS)
-        private val daemonsLabel = JLabel("Daemons:")
-        private var daemonsInfo = StringBuilder()
+        private val totalRAMLabel = JLabel()
+        private val freeRAMLabel = JLabel()
+        private val daemonsPanel = JPanel()
+        private val daemonsButtons = mutableMapOf<String, JButton>()
 
         init {
             panel.layout = layout
-
-            val osBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
-            val decimalFormat = DecimalFormat("#.##")
-
-            val totalRAMLabel = JLabel("Total RAM: ${getTotalRAM()} MB")
-            val freeRAMLabel = JLabel()
-
             panel.add(totalRAMLabel)
             panel.add(freeRAMLabel)
-            panel.add(daemonsLabel)
+            panel.add(daemonsPanel)
 
             fixedRateTimer(period = 5000) {
-                val freePhysicalMemorySize = osBean.freePhysicalMemorySize
-                val freeMemoryInMB = freePhysicalMemorySize.toDouble() / (1024 * 1024)
-                val freeRAMText = "Free RAM: ${decimalFormat.format(freeMemoryInMB)} MB"
-                SwingUtilities.invokeLater {
-                    freeRAMLabel.text = freeRAMText
-                    updateDaemonsInfo()
-                }
+                updateDaemonsInfo()
+                updateRAMInfo()
             }
         }
 
@@ -58,23 +45,44 @@ class MyToolWindowFactory : ToolWindowFactory {
             val reader = BufferedReader(InputStreamReader(process.inputStream))
             val daemons = reader.readText().lines().filter { it.contains("Daemon") }
 
-            daemonsInfo.clear()
+            daemonsPanel.removeAll()
+            daemonsButtons.clear()
+
             for (daemon in daemons) {
-                daemonsInfo.append(daemon).append("\n")
+                if (!daemonsButtons.containsKey(daemon)) {
+                    val daemonButton = JButton(daemon)
+                    daemonButton.addActionListener {
+                        displayDaemonInfo(daemon)
+                    }
+                    daemonsButtons[daemon] = daemonButton
+                    daemonsPanel.add(daemonButton)
+                }
             }
 
-            SwingUtilities.invokeLater {
-                daemonsLabel.text = "Daemons:\n$daemonsInfo"
-                panel.revalidate()
-                panel.repaint()
-            }
+            panel.revalidate()
+            panel.repaint()
+        }
+
+        private fun updateRAMInfo() {
+            val osBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
+            val decimalFormat = DecimalFormat("#.##")
+
+            val totalRAM = osBean.totalPhysicalMemorySize.toDouble() / (1024 * 1024)
+            val freeRAM = osBean.freePhysicalMemorySize.toDouble() / (1024 * 1024)
+
+            totalRAMLabel.text = "Total RAM: ${decimalFormat.format(totalRAM)} MB"
+            freeRAMLabel.text = "Free RAM: ${decimalFormat.format(freeRAM)} MB"
+        }
+
+        private fun displayDaemonInfo(daemonName: String) {
+            val dialog = JDialog()
+            val dialogPanel = JPanel()
+            dialogPanel.add(JLabel("Details for $daemonName"))
+            dialog.contentPane = dialogPanel
+            dialog.setSize(300, 150)
+            dialog.isVisible = true
         }
 
         fun getContent() = panel
-
-        private fun getTotalRAM(): Long {
-            val osBean = ManagementFactory.getOperatingSystemMXBean() as OperatingSystemMXBean
-            return osBean.totalPhysicalMemorySize / (1024 * 1024)
-        }
     }
 }
