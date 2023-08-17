97d5d711020097f531802ace7af24fca76d1fb5d
Svetlana Koreneva <corenewa.s@gmail.com>
2023-08-17 04:14:06 +0200

    fix adding action for plugin button

--- src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/DaemonsManagerAction.kt
+++ src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/DaemonsManagerAction.kt
@@ -3,7 +3,7 @@ package com.github.darmsteter.builddaemonsintellijmanagerplugin
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
-class DaemonsManagerAction : AnAction("") {
+class DaemonsManagerAction : AnAction("Build daemons manager plugin") {
     override fun actionPerformed(e: AnActionEvent) {
 
     }
