0f57fabeca5943db6e53ff4c4e1396479cb083e1
Svetlana Koreneva <corenewa.s@gmail.com>
2023-08-29 06:19:52 +0200

    change update data for daemons action

--- src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/DaemonTable.kt
+++ src/main/kotlin/com/github/darmsteter/builddaemonsintellijmanagerplugin/DaemonTable.kt
@@ -3,16 +3,18 @@ package com.github.darmsteter.builddaemonsintellijmanagerplugin
 import com.intellij.openapi.actionSystem.AnAction
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import java.util.*
 import javax.swing.JTable
 import javax.swing.table.DefaultTableModel
+import javax.swing.table.TableModel
+
 
 class DaemonTable(
     private val daemonsManagerAction: DaemonsManagerAction
 ) : JTable() {
-    init {
-        val model = DefaultTableModel(arrayOf("Daemon name", "RAM", "CPU"), 0)
-        this.model = model
+    override fun createDefaultDataModel() = DefaultTableModel(arrayOf("Daemon name", "RAM", "%CPU"), 0)
 
+    init {
         addMouseListener(object : MouseAdapter() {
             override fun mouseClicked(e: MouseEvent) {
                 if (e.clickCount == 1) {
@@ -27,14 +29,36 @@ class DaemonTable(
         })
     }
 
+    override fun setModel(dataModel: TableModel) {
+        if (model == null) {
+            super.setModel(dataModel)
+        } else {
+            error("Changing model is not allowed")
+        }
+    }
 
     fun updateData(daemonActions: Map<String, AnAction>) {
         val model = this.model as DefaultTableModel
-        model.rowCount = 0
+
+        val rowsToRemove = model.dataVector.filterNot { it[0] in daemonActions.keys }
+        model.dataVector.removeAll(rowsToRemove.toSet())
 
         for (daemon in daemonActions) {
             val daemonName = daemon.key
-            model.addRow(arrayOf(daemonName, "2GB", "10"))
+
+            val existing = model.dataVector.indexOfFirst { it[0] == daemonName }
+            if (existing != -1) {
+                model.dataVector[existing][1] = "0 MB"
+                model.dataVector[existing][2] = "0.0%"
+            } else {
+                model.addRow(Vector<String>().apply {
+                    add(daemonName)
+                    add("0 MB")
+                    add("0.0%")
+                })
+            }
         }
+
+        model.fireTableDataChanged()
     }
 }
